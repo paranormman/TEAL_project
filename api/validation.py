357b6e7ac0816fb517f46129b9623df15ca8dbd2
Manoj <manojshivaprakash10@gmail.com>
2021-04-11 09:40:00 +0530

    update 0.3

--- api/validation.py
+++ api/validation.py
@@ -1,14 +1,33 @@
 from django.core.exceptions import ValidationError
 import pandas as pd
+import logging
 
 
 
 def validate_file(value):
+    path = 'E:\\Django_proj\\restapi\\media\\Acc_time_ext.csv'
     value= str(value)
-    if value.endswith(".csv") != True: 
+    if value.endswith(".csv") == True:
+        try:
+            df = pd.read_csv(path)
+            n = len(df.columns.tolist())
+            if n == 0:
+                raise ValidationError(u'No Column to parse ')
+            elif n == 1:
+                raise ValidationError(u'No time value in the file ')
+            elif n == 2:
+                raise ValidationError(u'Missing amplitude value in the file')
+            else:
+                return value
+        except Exception as e:
+            logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+            raise ValidationError("Unable to upload CVS file. "+repr(e))
+        
+    else:
         raise ValidationError("Only CSV can be uploaded")
+
     # df=pd.read_csv(value)
     # if not ("time" in df.columns.tolist() and "amplitude" in df.columns.tolist()):
     #     raise ValidationError('CSV file must have "time" column and "amplitude column')
-    else:
-        return value
+    # else:
+    #     return value
