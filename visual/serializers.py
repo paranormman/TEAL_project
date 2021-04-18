e3bcc1619d2c585cc2e7ba93e7778a03e32079d5
Manoj <manojshivaprakash10@gmail.com>
2021-04-18 22:46:25 +0530

    update 0.5

--- visual/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SourceFile
-
-
-class FileSerializer(serializers.ModelSerializer):
-  class Meta():
-    model = SourceFile
-    fields = ('file', 'remark',)
