d82fede81894bf95e6eafa796ed6ed9b48c7d303
Manoj <manojshivaprakash10@gmail.com>
2021-04-19 15:14:10 +0530

    update 0.5

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import url
-from . views import FileViewSet, calculations
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'users', FileViewSet.as_view)
+from . views import FileView, calculations
 
 
 
-# urlpatterns = [
-#     url(r'^upload/$', FileView.as_view(), name='file_upload'),
-#     url(r'^calculate/$', calculations.as_view(), name='calculation'),
-# ]
+urlpatterns = [
+    url(r'^upload/$', FileView.as_view(), name='file_upload'),
+    url(r'^calculate/$', calculations.as_view(), name='calculation'),
+]
--- restapi/urls.py
+++ restapi/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
-from restapi.api.urls import router
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^file/', include('router.urls')),
+    url(r'^file/', include('api.urls')),
 ]
 
 if settings.DEBUG:
